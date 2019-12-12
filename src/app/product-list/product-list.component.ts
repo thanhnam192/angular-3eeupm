8942eb86cd447c237e10c60b1a58953927dd66a9
thanhnam192 <thanhnam192@gmail.com>
2019-12-12 14:31:30 +0700

    3 files modified, 6 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { products } from '../products';
 
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -12,6 +13,10 @@ export class ProductListComponent {
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+   onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
